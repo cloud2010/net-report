1c564324ce93f8059bf95a7af57f8ebca02f09ad
Capt.Liu <capt.liu@foxmail.com>
2017-12-15 13:40:13 +0800

    add sep. analysis

--- py-code/preference-analytics.py
+++ py-code/preference-analytics.py
@@ -4,7 +4,7 @@ Using pandas for data analysis
 """
 import os
 import time
-import numpy as np
+# import numpy as np
 import pandas as pd
 
 __author__ = 'Liu Min'
@@ -21,7 +21,7 @@ def main():
 
     # print(df.dtypes)
 
-    top_list = ['TOP ' + str(i) for i in range(1, 20)]
+    top_list = ['TOP ' + str(i) for i in range(1, 21)]
 
     # 各大学城大一至大四10月份APP使用情况按pv指标统计
     with pd.ExcelWriter('../result/10月份APP分析.xlsx') as xls:
@@ -29,12 +29,25 @@ def main():
             for age in range(1995, 1999):
                 app = df[(df['校区'] == school) & (df['出生年份'] == age) & (df['统计月份'] == 201710)]
                 for sex in ['男', '女']:
-                    app_top=app[app['性别'] == sex].nlargest(20, columns='uv').tail(19)
+                    app_top=app[app['性别'] == sex].nlargest(20, columns='uv')
                     app_top['TOP']=top_list
                     sum_stu = df_b[(df_b['性别'] == sex) & (df_b['出生年份'] == age) & (df_b['校区'] == school)]['人数']
                     app_top['渗透率'] = app_top['uv'] / int(sum_stu)
                     app_top['月平均访问人次'] = app_top['pv'] / int(sum_stu)
                     app_top.to_excel(xls, '{0}_10月_{1}_{2}_APP_T20'.format(school, sex, age), index=False)
+    
+    # 各大学城大一至大四9月份APP使用情况按pv指标统计
+    with pd.ExcelWriter('../result/9月份APP分析.xlsx') as xls:
+        for school in ['临港大学城', '杨浦大学城', '闵行大学城', '松江大学城', '南汇大学城']:
+            for age in range(1995, 1999):
+                app = df[(df['校区'] == school) & (df['出生年份'] == age) & (df['统计月份'] == 201709)]
+                for sex in ['男', '女']:
+                    app_top=app[app['性别'] == sex].nlargest(20, columns='uv')
+                    app_top['TOP']=top_list
+                    sum_stu = df_b[(df_b['性别'] == sex) & (df_b['出生年份'] == age) & (df_b['校区'] == school)]['人数']
+                    app_top['渗透率'] = app_top['uv'] / int(sum_stu)
+                    app_top['月平均访问人次'] = app_top['pv'] / int(sum_stu)
+                    app_top.to_excel(xls, '{0}_9月_{1}_{2}_APP_T20'.format(school, sex, age), index=False)
 
 if __name__ == '__main__':
     start_time = time.time()
