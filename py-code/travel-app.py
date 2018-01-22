663606db274e461c04544cd8f23dc4558182545d
Capt.Liu <capt.liu@foxmail.com>
2018-01-22 22:10:20 +0800

    instead str replace re.str

--- py-code/travel-app.py
+++ py-code/travel-app.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 Using pandas for data analysis
-旅游美食APP 分类汇总分析
+生活出行APP 分类汇总分析
 """
 import os
 import time
@@ -14,6 +14,17 @@ __author__ = 'Liu Min'
 APP_DATA = '校园app数据.xlsx'
 BASE_DATA = '校园基础数据.xls'
 
+def scale_pv(x):
+    '''
+    缩放PV值
+    '''
+    s_pv = x['pv']
+    if x['app名称/类型'] == '高德地图':
+        s_pv = x['pv'] / 6
+    if x['app名称/类型'] == '百度地图':
+        s_pv = x['pv'] / 3
+    return s_pv
+
 def main():
     '''
     主函数
@@ -23,33 +34,37 @@ def main():
     # 人数数据
     df_b = pd.read_excel(os.path.join('../dataset', BASE_DATA))
 
-    pattern = re.compile(u'饿了么|美团|口碑|点评|外卖|携程|去哪儿|旅行|单车')
+    pattern = re.compile(u'地图|饿了么|美团|口碑|点评|外卖|携程|去哪儿|旅行|单车|ofo')
     # 数据清洗排除爬虫兴趣标注
     df = df[(df['app名称/类型'].str.contains(pattern) == 1) & (df['统计月份'] == 201710)]
+     # 数值缩放
+    df['pv'] = df.apply(scale_pv, axis='columns')
     
     # 按性别分析
-    with pd.ExcelWriter('../result/旅游美食APP按性别分类汇总统计.xlsx') as xls:
-        for school in ['临港大学城', '杨浦大学城', '闵行大学城', '松江大学城']:
+    with pd.ExcelWriter('../result/生活出行APP按性别分类汇总统计.xlsx') as xls:
+        for school in [u'宝山|临港|曹路', u'杨浦|闵行', u'松江']:
             for sex in ['男', '女']:
-                app = df[(df['校区'] == school) & (df['性别'] == sex)]
+                pattern_s = re.compile(school)
+                app = df[(df['校区'].str.contains(pattern_s) == 1) & (df['性别'] == sex)]
                 g_df = app.groupby(['app名称/类型']).agg({'出生年份' : np.size, 'pv' : np.sum, 'uv' : np.sum}).nlargest(20, columns='pv')
-                sum_stu = df_b[(df_b['性别'] == sex) & (df_b['校区'] == school)]['人数'].sum()
+                sum_stu = df_b[(df_b['性别'] == sex) & (df_b['校区'].str.contains(pattern_s) == 1)]['人数'].sum()
                 g_df['渗透率'] = g_df['uv'] / int(sum_stu)
                 g_df['月均访问人次'] = g_df['pv'] / int(sum_stu)
                 g_df['学生数'] = int(sum_stu)
-                g_df.to_excel(xls, '{0}_旅游美食_{1}_SEX_APP_T20'.format(school, sex), index=True)
+                g_df.to_excel(xls, '{0}_生活出行_{1}_SEX_APP_T20'.format(school, sex), index=True)
     
     # 按年级分析
-    with pd.ExcelWriter('../result/旅游美食APP按年级分类汇总统计.xlsx') as xls:
-        for school in ['临港大学城', '杨浦大学城', '闵行大学城', '松江大学城']:
+    with pd.ExcelWriter('../result/生活出行APP按年级分类汇总统计.xlsx') as xls:
+        for school in [u'宝山|临港|曹路', u'杨浦|闵行', u'松江']:
             for age in range(1995, 1999):
-                app = df[(df['校区'] == school) & (df['出生年份'] == age)]
+                pattern_s = re.compile(school)
+                app = df[(df['校区'].str.contains(pattern_s) == 1) & (df['出生年份'] == age)]
                 g_df = app.groupby(['app名称/类型']).agg({'性别' : np.size, 'pv' : np.sum, 'uv' : np.sum}).nlargest(20, columns='pv')
-                sum_stu = df_b[(df_b['出生年份'] == age) & (df_b['校区'] == school)]['人数'].sum()
+                sum_stu = df_b[(df_b['出生年份'] == age) & (df_b['校区'].str.contains(pattern_s) == 1)]['人数'].sum()
                 g_df['渗透率'] = g_df['uv'] / int(sum_stu)
                 g_df['月均访问人次'] = g_df['pv'] / int(sum_stu)
                 g_df['学生数'] = int(sum_stu)
-                g_df.to_excel(xls, '{0}_旅游美食_{1}_AGE_APP_T20'.format(school, age), index=True)
+                g_df.to_excel(xls, '{0}_生活出行_{1}_AGE_APP_T20'.format(school, age), index=True)
 
 if __name__ == '__main__':
     start_time = time.time()
