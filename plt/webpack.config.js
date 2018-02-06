7377caa6ac91a557381902e4af88fcfb6a552a2d
Min Liu <capt.liu@foxmail.com>
2018-02-06 23:12:06 +0800

    eslint fix

--- plt/webpack.config.js
+++ plt/webpack.config.js
@@ -1,13 +1,13 @@
-const path = require('path');
-const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin'); // 加载HTML插件
-const CleanWebpackPlugin = require('clean-webpack-plugin'); // 清理dist文件夹
-const ExtractTextPlugin = require('extract-text-webpack-plugin'); // 分离CSS和JS文件
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin') // 加载HTML插件
+const CleanWebpackPlugin = require('clean-webpack-plugin') // 清理dist文件夹
+const ExtractTextPlugin = require('extract-text-webpack-plugin') // 分离CSS和JS文件
 
 module.exports = {
   devtool: 'inline-npsource-map',
   entry: {
-    main: './src/main.js',
+    main: './src/main.js'
     // charts: './src/plt.js'
   },
   output: {
@@ -16,38 +16,38 @@ module.exports = {
   },
   module: {
     rules: [{
-        test: /\.css$/,
-        use: ExtractTextPlugin.extract({
-          fallback: "style-loader", // 编译后用什么loader来提取css文件
-          use: "css-loader" // 需要什么样的loader去编译文件
-        })
-      },
-      {
-        test: /\.js$/,
-        use: [
-          'babel-loader' // 加速JS编译
-        ],
-        include: path.resolve(__dirname, "src"),
-        exclude: path.resolve(__dirname, "node_modules")
-      }
+      test: /\.css$/,
+      use: ExtractTextPlugin.extract({
+        fallback: 'style-loader', // 编译后用什么loader来提取css文件
+        use: 'css-loader' // 需要什么样的loader去编译文件
+      })
+    },
+    {
+      test: /\.js$/,
+      use: [
+        'babel-loader' // 加速JS编译
+      ],
+      include: path.resolve(__dirname, 'src'),
+      exclude: path.resolve(__dirname, 'node_modules')
+    }
     ]
 
   },
   devServer: {
-    contentBase: "./dist", //本地服务器所加载的页面所在的目录
-    historyApiFallback: true, //不跳转
-    inline: true //实时刷新
+    contentBase: './dist', // 本地服务器所加载的页面所在的目录
+    historyApiFallback: true, // 不跳转
+    inline: true // 实时刷新
   },
   plugins: [
     new CleanWebpackPlugin(['dist']), // 清理dist文件夹
     new webpack.BannerPlugin('Author: capt.liu@foxmail.com'),
     new HtmlWebpackPlugin({
       // new一个这个插件的实例，并传入相关的参数
-      template: __dirname + "/src/view/index.tmpl.html"
+      template: path.resolve(__dirname, '/src/view/index.tmpl.html')
     }),
     new webpack.optimize.OccurrenceOrderPlugin(),
     new ExtractTextPlugin('style.css') // 加载插件
     // 压缩JS
     // new webpack.optimize.UglifyJsPlugin()
-  ],
-};
+  ]
+}
