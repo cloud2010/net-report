e816e96683577bd63147ac45968b40ada2d5a27b
Min@Home <liumin@shmtu.edu.cn>
2019-06-19 21:47:02 +0800

    update vote

--- post-test.py
+++ post-test.py
@@ -4,6 +4,7 @@
 """
 import http.client
 import time
+import json
 
 
 def vote_number_four(counts):
@@ -19,13 +20,22 @@ def vote_number_four(counts):
         conn.request("POST", "/Vote/GetAdd", payload, headers)
         res = conn.getresponse()
         data = res.read()
-        print('\n已投票:{0}张'.format(i))
-        print(data.decode("utf-8"))
+        print('\n已投票:{0}张\n'.format(i))
+        result_json = json.loads(data)
+        print('投票状态：', result_json)
+    
+    # 查看最新票数
+    conn.request("GET", "/Vote/GetVoteCountList")
+    res = conn.getresponse()
+    data = res.read()
+    result_json = json.loads(data)
+    nums_vote = result_json['data'][3]
+    print("\n组别：{0} ==> 当前票数：{1}".format(nums_vote['VoteID'], nums_vote['Count']))
 
 
 if __name__ == '__main__':
     start_time = time.time()
-    vote_number_four(278)
+    vote_number_four(589)
     end_time = time.time()  # 程序结束时间
     print("\n[Finished in: {0:.6f} mins = {1:.6f} seconds]".format(
         ((end_time - start_time) / 60), (end_time - start_time)))
