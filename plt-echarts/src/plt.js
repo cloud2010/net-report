041b9ee60cffba69a02beb2bd2577c8f8a09fa4b
liumin@home <liumin@shmtu.edu.cn>
2020-03-18 17:57:24 +0800

    update webpack to 4.x

--- plt-echarts/src/plt.js
+++ plt-echarts/src/plt.js
@@ -2,16 +2,6 @@
  * 绘图代码
  */
 
-var dataset = require('../public/09-m-1-app-t20.json')
-// 构建数据
-var pltData = []
-var pltValue = []
-dataset.forEach(element => {
-  pltData.push(element[1])
-  pltValue.push(element[6])
-})
-var echarts = require('echarts')
-
 // 引入 ECharts 主模块
 var echarts = require('echarts/lib/echarts')
 // 引入柱状图
@@ -23,23 +13,37 @@ require('echarts/lib/component/title')
 // 基于准备好的dom，初始化echarts实例
 var divRender = document.querySelector('#main')
 divRender.setAttribute('style', 'width:1200px; height:600px;')
-
+// 基于准备好的dom，初始化echarts实例
 var myChart = echarts.init(divRender)
 // 绘制图表
 myChart.setOption({
   title: {
-    text: '临港大学城9月份大一男生APP情况'
+    text: 'ECharts 入门示例'
   },
   tooltip: {},
+  toolbox: {
+    show: true,
+    itemSize: 20,
+    itemGap: 30,
+    right: 50,
+    feature: {
+      dataView: { show: true },
+      saveAsImage: {
+        show: true,
+        type: 'png',
+        pixelRatio: 2
+      }
+    }
+  },
   xAxis: {
-    data: pltData
+    data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
   },
   yAxis: {},
   series: [
     {
-      name: 'UV值',
+      name: '销量',
       type: 'bar',
-      data: pltValue
+      data: [5, 20, 36, 10, 10, 20]
     }
   ]
 })
--- plt-echarts/webpack.config.js
+++ plt-echarts/webpack.config.js
@@ -1,37 +1,55 @@
 const path = require('path')
 const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin') // 加载HTML插件
-const CleanWebpackPlugin = require('clean-webpack-plugin') // 清理dist文件夹
-const ExtractTextPlugin = require('extract-text-webpack-plugin') // 分离CSS和JS文件
+const { CleanWebpackPlugin } = require('clean-webpack-plugin') // 清理dist文件夹
+const MiniCssExtractPlugin = require('mini-css-extract-plugin') // webpack 4.x CSS和JS分离插件
+// const ExtractTextPlugin = require('extract-text-webpack-plugin') // 分离CSS和JS文件 webpack 4.x 移除
 
 module.exports = {
   devtool: 'inline-npsource-map',
   entry: {
-    main: './src/main.js'
-    // charts: './src/plt.js'
+    main: './src/main.js',
+    charts: './src/plt.js'
   },
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
   module: {
-    rules: [{
-      test: /\.css$/,
-      use: ExtractTextPlugin.extract({
-        fallback: 'style-loader', // 编译后用什么loader来提取css文件
-        use: 'css-loader' // 需要什么样的loader去编译文件
-      })
-    },
-    {
-      test: /\.js$/,
-      use: [
-        'babel-loader' // 加速JS编译
-      ],
-      include: path.resolve(__dirname, 'src'),
-      exclude: path.resolve(__dirname, 'node_modules')
-    }
+    rules: [
+      {
+        // test: /\.css$/,
+        // use: ExtractTextPlugin.extract({
+        //   fallback: 'style-loader',
+        //   use: 'css-loader'
+        // })
+        /**
+         * 编译后用什么loader来提取css文件
+         * 需要什么样的loader去编译文件
+         */
+        test: /\.css$/i,
+        // use: ['style-loader', 'css-loader']
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader'
+          }
+        ]
+      }
     ]
-
   },
   devServer: {
     contentBase: path.resolve(__dirname, 'dist'), // 本地服务器所加载的页面所在的目录
@@ -39,14 +57,17 @@ module.exports = {
     inline: true // 实时刷新
   },
   plugins: [
-    new CleanWebpackPlugin(['dist']), // 清理dist文件夹
+    new CleanWebpackPlugin({
+      cleanAfterEveryBuildPatterns: ['dist']
+    }), // 清理dist文件夹
     new webpack.BannerPlugin('Author: capt.liu@foxmail.com'),
     new HtmlWebpackPlugin({
       // new一个这个插件的实例，并传入相关的参数
       template: path.resolve(__dirname, './src/view/index.tmpl.html')
     }),
     new webpack.optimize.OccurrenceOrderPlugin(),
-    new ExtractTextPlugin('style.css') // 加载插件
+    new MiniCssExtractPlugin()
+    // new ExtractTextPlugin('style.css') // 加载插件
     // 压缩JS
     // new webpack.optimize.UglifyJsPlugin()
   ]
